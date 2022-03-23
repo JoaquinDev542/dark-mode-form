83194642d93de1d31c82922f323d9aa2422bbf77
Joaquín Uriel García <82168815+JoaquinDev542@users.noreply.github.com>
2022-03-23 17:01:19 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-let darkModeIcon = document.getElementById("dark-mode__icon");
-let body = document.querySelector("body");
-let title = document.querySelectorAll(".texts");
+const $emailInput = document.querySelector(".input--email");
+const $passwordInput = document.querySelector(".input--password");
+const $textarea = document.querySelector(".textarea");
+const $submit = document.querySelector(".btn");
 
-darkModeIcon.addEventListener("click" , () => {
-    body.classList.toggle("active");
-})
+const regExEmail = /^(([^<>()\[\]\\.,:\s@"]+(\.[^<>()\[\]\\.,:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const regExPassword = /(?=(.*[0-9]))((?=.*[A-Za-z0-9])(?=.*[A-Z])(?=.*[a-z]))^.{8,}$/;
