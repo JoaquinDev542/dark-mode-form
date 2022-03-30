4e5f26a9fe3a33590343efa702f0e8a095998d5d
Joaquín Uriel García <82168815+JoaquinDev542@users.noreply.github.com>
2022-03-30 16:34:56 +0200

    improve

--- main.js
+++ main.js
@@ -6,3 +6,8 @@ const $submit = document.querySelector(".btn");
 const regExEmail = /^(([^<>()\[\]\\.,:\s@"]+(\.[^<>()\[\]\\.,:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const regExPassword = /(?=(.*[0-9]))((?=.*[A-Za-z0-9])(?=.*[A-Z])(?=.*[a-z]))^.{8,}$/;
 
+$submit.addEventListener("click" , () => {
+    
+    
+
+})
