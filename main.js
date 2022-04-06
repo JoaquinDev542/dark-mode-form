8c499e814dee6c0d21e2a94719935b8c5fad2906
Joaquín Uriel García <82168815+JoaquinDev542@users.noreply.github.com>
2022-04-06 18:05:58 +0200

    Update main.js

--- main.js
+++ main.js
@@ -6,40 +6,36 @@ const $submit = document.querySelector(".btn");
 const regExEmail = /^(([^<>()\[\]\\.,:\s@"]+(\.[^<>()\[\]\\.,:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const regExPassword = /(?=(.*[0-9]))((?=.*[A-Za-z0-9])(?=.*[A-Z])(?=.*[a-z]))^.{8,}$/;
 
-
 $submit.addEventListener("click" , () => {
+
+
     
-    if(regExEmail.test($emailInput.value) === true  ){
-        $emailInput.style.border = "2px solid green";
+    if(regExEmail.test($emailInput.value) === true  ) {
+        $emailInput.style.border = "1px solid #d6d7d7";
     } else {
-       $emailInput.style.border = "2px solid red";
+        $emailInput.style.border = "2px solid red";
     }
 
     if(regExPassword.test($passwordInput.value) === true) {
-        $passwordInput.style.border = "2px solid green";
+        $passwordInput.style.border = "1px solid #d6d7d7";
     } else {
         $passwordInput.style.border = "2px solid red";
     }
 
     if($textarea.value.length >= 9 ) {
-        $textarea.style.border = "2px solid green";
+        $textarea.style.border = "1px solid #d6d7d7";
     } else {
-        $textarea.style.border = "2px solid red";
+        $textarea.style.border = "2px solid red";  
     }
 
     if(regExEmail.test($emailInput.value) === true && regExPassword.test($passwordInput.value) === true && $textarea.value.length >= 9 ) {
-
-        let messageCorrect = document.createElement("p");
-        messageCorrect.textContent = "El formulario se ha completado correctamente."; 
-
+        alert("El formulario se ha completado correctamente");
+        $emailInput.value = "";
+        $passwordInput.value = "";
+        $textarea.value = "";
 
     } else {
-      alert("El formulario NO se ha completado. Por favor, complete todo los inputs.");
+      alert("El formulario NO se ha completado. Por favor, complete todo los inputs con borde rojo.");
 
    }
-        
-
-    
 })
-
-// mayuscula, minuscula, numero, speacil caracter
